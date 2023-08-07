1b56b427950fb28d1e488e05ac03730faaf7f5ac
GYoung <ytok1108@gmail.com>
2023-08-07 16:07:43 +0900

    test 2column

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,164 @@
+// #![windows_subsystem = "windows"]
+
+// use druid::widget::{Controller, CrossAxisAlignment, Painter};
+// use druid::widget::{Flex, Label};
+// use druid::{
+//     theme, AppLauncher, Color, Data, Env, Event, EventCtx, Lens, RenderContext, Widget, WidgetExt,
+//     WindowDesc,
+// };
+
+// use chrono::Utc;
+// use chrono_tz::America::New_York;
+
+// use druid::widget::ControllerHost;
+// use std::time::Duration;
+
+// #[derive(Clone, Data, Lens)]
+// struct TimeState {
+//     time: String,
+// }
+
+// #[derive(Clone, Data, Lens)]
+// struct TimeState2 {
+//     time: String,
+// }
+
+
+// #[derive(Clone, Data)]
+// struct TimeController;
+
+// #[derive(Clone, Data)]
+// struct TimeController2;
+
+// impl<W: Widget<TimeState>> Controller<TimeState, W> for TimeController {
+//     fn event(
+//         &mut self,
+//         child: &mut W,
+//         ctx: &mut EventCtx,
+//         event: &Event,
+//         data: &mut TimeState,
+//         env: &Env,
+//     ) {
+//         match event {
+//             Event::WindowConnected => {
+//                 let timer_id = ctx.request_timer(Duration::from_secs(1));
+//                 ctx.set_handled();
+//             }
+//             Event::Timer(timer_id) => {
+//                 data.time = data.get_time();
+//                 ctx.request_paint();
+//                 let timer_id = ctx.request_timer(Duration::from_secs(1));
+//                 ctx.set_handled();
+//             }
+//             _ => {}
+//         }
+//         child.event(ctx, event, data, env);
+//     }
+// }
+
+// impl<W: Widget<TimeState2>> Controller<TimeState2, W> for TimeController2 {
+//     fn event(
+//         &mut self,
+//         child: &mut W,
+//         ctx: &mut EventCtx,
+//         event: &Event,
+//         data: &mut TimeState2,
+//         env: &Env,
+//     ) {
+//         match event {
+//             Event::WindowConnected => {
+//                 let timer_id = ctx.request_timer(Duration::from_secs(1));
+//                 ctx.set_handled();
+//             }
+//             Event::Timer(timer_id) => {
+//                 data.time = data.get_time();
+//                 ctx.request_paint();
+//                 let timer_id = ctx.request_timer(Duration::from_secs(1));
+//                 ctx.set_handled();
+//             }
+//             _ => {}
+//         }
+//         child.event(ctx, event, data, env);
+//     }
+// }
+
+// fn flex_row<T: Data>(
+//     w1: impl Widget<T> + 'static,
+//     w2: impl Widget<T> + 'static,
+//     // w3: impl Widget<T> + 'static,
+//     // w4: impl Widget<T> + 'static,
+// ) -> impl Widget<T> {
+//     Flex::row()
+//         .with_flex_child(w1, 1.0)
+//         .with_spacer(1.0)
+//         .with_flex_child(w2, 1.0)
+//     // .with_spacer(1.0)
+//     // .with_flex_child(w3, 1.0)
+//     // .with_spacer(1.0)
+//     // .with_flex_child(w4, 1.0)
+// }
+
+// fn build_root_widget() -> impl Widget<TimeState> {
+//     let display = Label::new(|data: &String, _env: &_| data.clone())
+//         .with_text_size(24.0)
+//         .lens(TimeState::time)
+//         .padding(5.0);
+
+//     let display = ControllerHost::new(display, TimeController);
+    
+//     let display2 = Label::new(|data: &String, _env: &_| data.clone())
+//         .with_text_size(24.0)
+//         .lens(TimeState2::time)
+//         .padding(5.0);
+
+//     let display2 = ControllerHost::new(display2, TimeController2);
+
+//     Flex::row()
+//         .with_flex_spacer(0.2)
+//         .with_child(display)
+//         .with_flex_spacer(0.2)
+//         .with_child(display2)
+// }
+
+// impl TimeState {
+//     fn get_time(&mut self) -> String {
+//         Utc::now().with_timezone(&New_York).to_string()
+//     }
+// }
+
+
+// impl TimeState2 {
+//     fn get_time(&mut self) -> String {
+//         Utc::now().with_timezone(&New_York).to_string()
+//     }
+// }
+
+// pub fn main() {
+//     // describe the main window
+//     let main_window = WindowDesc::new(build_root_widget())
+//         .title("NewYork_Time: EST!")
+//         .window_size((349.0, 200.0))
+//         .resizable(false);
+
+//     // create the initial app state
+//     let initial_state: TimeState = TimeState {
+//         time: "".to_string(),
+//         // }
+
+//         // let TimeState2 = TimeState2 {
+//         // time: "".to_string(),
+//         // };
+//     };
+
+//     // start the application. Here we pass in the application state.
+//     AppLauncher::with_window(main_window)
+//         .log_to_console()
+//         .launch(initial_state)
+//         .expect("Failed to launch application");
+// }
+
+
+
 #![windows_subsystem = "windows"]
 
 use druid::widget::{Controller, CrossAxisAlignment, Painter};
@@ -9,121 +170,68 @@ use druid::{
 
 use chrono::Utc;
 use chrono_tz::America::New_York;
+use chrono_tz::Asia::Seoul;
 
 use druid::widget::ControllerHost;
 use std::time::Duration;
 
 #[derive(Clone, Data, Lens)]
 struct TimeState {
-    time: String,
+    time_ny: String,
+    time_kr: String,
 }
 
-#[derive(Clone, Data)]
-struct TimeController;
-
-impl<W: Widget<TimeState>> Controller<TimeState, W> for TimeController {
-    fn event(
-        &mut self,
-        child: &mut W,
-        ctx: &mut EventCtx,
-        event: &Event,
-        data: &mut TimeState,
-        env: &Env,
-    ) {
-        match event {
-            Event::WindowConnected => {
-                let timer_id = ctx.request_timer(Duration::from_secs(1));
-                ctx.set_handled();
-            }
-            Event::Timer(timer_id) => {
-                data.time = data.get_time();
-                ctx.request_paint();
-                let timer_id = ctx.request_timer(Duration::from_secs(1));
-                ctx.set_handled();
-            }
-            _ => {}
-        }
-        child.event(ctx, event, data, env);
+impl TimeState {
+    fn get_time(&mut self) -> String {
+        let raw_ny_time = Utc::now().with_timezone(&New_York).to_string();
+        return format!("NewYork_Time: \n {:.19}", raw_ny_time);
     }
-}
 
-fn op_button_label(op: char, label: String) -> impl Widget<TimeState> {
-    let painter = Painter::new(|ctx, _, env| {
-        let bounds = ctx.size().to_rect();
-
-        ctx.fill(bounds, &env.get(theme::PRIMARY_DARK));
-
-        if ctx.is_hot() {
-            ctx.stroke(bounds.inset(-0.5), &Color::WHITE, 1.0);
-        }
-
-        if ctx.is_active() {
-            ctx.fill(bounds, &env.get(theme::PRIMARY_LIGHT));
-        }
-    });
-
-    Label::new(label)
-        .with_text_size(24.)
-        .center()
-        .background(painter)
-        .expand()
-}
-
-fn op_button(op: char) -> impl Widget<TimeState> {
-    op_button_label(op.clone(), op.to_string())
+    fn get_time_kr(&mut self) -> String {
+        let raw_kr_time = Utc::now().with_timezone(&Seoul).to_string();
+        return format!("_Korea_Time: \n {:.19}", raw_kr_time);
+    }
 }
 
 fn flex_row<T: Data>(
     w1: impl Widget<T> + 'static,
     w2: impl Widget<T> + 'static,
-    // w3: impl Widget<T> + 'static,
-    // w4: impl Widget<T> + 'static,
 ) -> impl Widget<T> {
     Flex::row()
         .with_flex_child(w1, 1.0)
         .with_spacer(1.0)
         .with_flex_child(w2, 1.0)
-    // .with_spacer(1.0)
-    // .with_flex_child(w3, 1.0)
-    // .with_spacer(1.0)
-    // .with_flex_child(w4, 1.0)
 }
 
 fn build_root_widget() -> impl Widget<TimeState> {
-    let display = Label::new(|data: &String, _env: &_| data.clone())
+    let display_ny = Label::new(|data: &String, _env: &_| data.clone())
         .with_text_size(24.0)
-        .lens(TimeState::time)
+        .lens(TimeState::time_ny)
         .padding(5.0);
 
-    let display = ControllerHost::new(display, TimeController);
+    let display_kr = Label::new(|data: &String, _env: &_| data.clone())
+        .with_text_size(24.0)
+        .lens(TimeState::time_kr)
+        .padding(5.0);
 
-    Flex::column()
+    Flex::row()
         .with_flex_spacer(0.2)
-        .with_child(display)
+        .with_child(display_ny)
         .with_flex_spacer(0.2)
-        .cross_axis_alignment(CrossAxisAlignment::End)
-        .with_flex_child(
-            flex_row(op_button_label('c', "CE".to_string()), op_button('C')),
-            1.0,
-        )
-}
-
-impl TimeState {
-    fn get_time(&mut self) -> String {
-        Utc::now().with_timezone(&New_York).to_string()
-    }
+        .with_child(display_kr)
 }
 
 pub fn main() {
     // describe the main window
     let main_window = WindowDesc::new(build_root_widget())
-        .title("NewYork_Time: EST!")
+        .title("NewYork_Time: EST! Korean_Time: KST!")
         .window_size((349.0, 200.0))
         .resizable(false);
 
     // create the initial app state
-    let initial_state: TimeState = TimeState {
-        time: "".to_string(),
+    let initial_state = TimeState {
+        time_ny: "".to_string(),
+        time_kr: "".to_string(),
     };
 
     // start the application. Here we pass in the application state.
